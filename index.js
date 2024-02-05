f250a6604e58670fd0db28e57ae051162f5ffc3b
makfiz <99556605+makfiz@users.noreply.github.com>
2024-02-05 20:07:48 +0200

    up

--- index.js
+++ index.js
@@ -1,49 +1,71 @@
 const robot = require('robotjs');
 const fs = require('fs');
-const ffi = require('ffi-napi');
+// const ffi = require('ffi-napi');
 const jimp = require('jimp');
 const Tesseract = require('tesseract.js');
+const {  windowPosition,
+  id,
+  width,
+  height} = require('./quant')
+  console.log(windowPosition)
+  console.log(width)
+  console.log(height)
+// const windowPosition = {x:79, y:147}
+// const width = 10
+// const height = 10
+
+  
+// const { execSync } = require('child_process');
 // const screenSize = robot.getScreenSize();
 // console.log(screenSize);
 // const worker = await createWorker('eng');
-const targetWindowTitle = 'Quant';
+// const targetWindowTitle = 'Quant';
 
-const user32 = new ffi.Library('user32', {
-  FindWindowA: ['int', ['string', 'string']],
-  GetWindowRect: ['bool', ['int', 'pointer']],
-  GetWindowTextA: ['int', ['int', 'string', 'int']],
-});
+// const user32 = new ffi.Library('user32', {
+//   FindWindowA: ['int', ['string', 'string']],
+//   GetWindowRect: ['bool', ['int', 'pointer']],
+//   GetWindowTextA: ['int', ['int', 'string', 'int']],
+// });
 
 // Function to get the handle of the window by title
-function getWindowHandleByTitle(title) {
-  return user32.FindWindowA(null, title);
-}
+// function getWindowHandleByTitle(title) {
+//   return user32.FindWindowA(null, title);
+// }
+// function getWindowIdByTitle(title) {
+//   const command = `xdotool search --name "${title}"`;
+//   const result = execSync(command, { encoding: 'utf-8' });
+//   const windowId = result.trim();
+//   return windowId;
+// }
+
+// Пример использования
+
 
 // Function to get the window rectangle (position and size)
-function getWindowRect(handle) {
-  const rectBuffer = Buffer.alloc(16); // 4 int'а по 4 байта
-  user32.GetWindowRect(handle, rectBuffer);
+// function getWindowRect(handle) {
+//   const rectBuffer = Buffer.alloc(16); // 4 int'а по 4 байта
+//   user32.GetWindowRect(handle, rectBuffer);
 
-  const rect = [];
-  for (let i = 0; i < 4; i++) {
-    rect.push(rectBuffer.readInt32LE(i * 4));
-  }
+//   const rect = [];
+//   for (let i = 0; i < 4; i++) {
+//     rect.push(rectBuffer.readInt32LE(i * 4));
+//   }
 
-  return {
-    left: rect[0],
-    top: rect[1],
-    right: rect[2],
-    bottom: rect[3],
-  };
-}
+//   return {
+//     left: rect[0],
+//     top: rect[1],
+//     right: rect[2],
+//     bottom: rect[3],
+//   };
+// }
 
-async function performOCRAndFindWords(rect) {
+async function performOCRAndFindWords() {
   return new Promise((resolve, reject) => {
     const screen = robot.screen.capture(
-      rect.left,
-      rect.top,
-      rect.right - rect.left,
-      rect.bottom - rect.top
+      windowPosition.x,
+      windowPosition.y-50,
+      width,
+      height
     );
 
     new jimp(screen.width, screen.height, async function (err, img) {
@@ -62,7 +84,7 @@ async function performOCRAndFindWords(rect) {
       logger: info => console.log(),
     }).then(({ data: { text, lines } }) => {
       // Выведите распознанный текст
-      // console.log('Распознанный текст:', text);
+      console.log('Распознанный текст:', text);
 
       // Создайте объект для хранения слов и их координат
       const wordsWithCoordinates = [];
@@ -94,13 +116,13 @@ async function performOCRAndFindWords(rect) {
   });
 }
 
-async function performOCRAndFindLines(rect) {
+async function performOCRAndFindLines() {
   return new Promise((resolve, reject) => {
     const screen = robot.screen.capture(
-      rect.left,
-      rect.top,
-      rect.right - rect.left,
-      rect.bottom - rect.top
+      windowPosition.x,
+      windowPosition.y-50,
+      width,
+      height
     );
 
     new jimp(screen.width, screen.height, async function (err, img) {
@@ -135,7 +157,7 @@ async function performOCRAndFindLines(rect) {
       // Выведите объект с словами и их координатами
 
       // Удалите временный файл снимка
-      // fs.unlinkSync('screen.png');
+      fs.unlinkSync('screen.png');
       resolve(linesFinded);
       // return wordsWithCoordinates;
     });
@@ -182,30 +204,104 @@ async function performOCRAndFindLines(rect) {
 //     });
 //   });
 // }
+// async function findAndClickBot () {
+//   const targetWord = 'Bot'; // Текст, который мы ищем
+
+//   const wordsWithCoordinates = await performOCRAndFindWords();
+//   console.log('Слова с координатами:', wordsWithCoordinates);
+//   const foundObject = wordsWithCoordinates.find(
+//     obj => obj.text === targetWord
+//   );
+
+//   if (foundObject) {
+//     console.log('Найден объект:', foundObject);
+//     const { left, top } = foundObject.coordinates;
+//     await new Promise((resolve) => {
+//       robot.moveMouse(windowPosition.x + left, windowPosition.y-50 + top);
+//       robot.mouseClick('left');
+//       setTimeout(() => {
+//         resolve();
+//       }, 1000);
+//     });
+    
+   
+    
+//   } 
+// }
+// async function findAndClickDownload () {
+//   const targetWord = 'Download'; // Текст, который мы ищем
+
+//   const wordsWithCoordinates = await performOCRAndFindWords();
+//   console.log('Слова с координатами:', wordsWithCoordinates);
+//   const foundObject = wordsWithCoordinates.find(
+//     obj => obj.text === targetWord
+//   );
+
+//   if (foundObject) {
+//     console.log('Найден объект:', foundObject);
+//     const { left, top } = foundObject.coordinates;
+//     await new Promise((resolve) => {
+//       robot.moveMouse(windowPosition.x + left, windowPosition.y-50 + top);
+//       robot.mouseClick('left');
+//       setTimeout(() => {
+//         resolve();
+//       }, 1000);
+//     });
+    
+   
+    
+//   } 
+// }
+
+async function findAndClick (name) {
+  const targetWord = name; // Текст, который мы ищем
+
+  const wordsWithCoordinates = await performOCRAndFindWords();
+  console.log('Слова с координатами:', wordsWithCoordinates);
+  const foundObject = wordsWithCoordinates.find(
+    obj => obj.text === targetWord
+  );
+
+  if (foundObject) {
+    console.log('Найден объект:', foundObject);
+    const { left, top } = foundObject.coordinates;
+    await new Promise((resolve) => {
+      robot.moveMouse(windowPosition.x + left, windowPosition.y-50 + top);
+      robot.mouseClick('left');
+      setTimeout(() => {
+        resolve();
+      }, 1000);
+    });
+    
+   
+    
+  } 
+}
+
+async function runBot () {
+  await findAndClick("Bot")
+  await findAndClick("Download")
+ await   new Promise((resolve) => {
+  setTimeout(async ()=>{
+    
+    await findAndClick("Bot")
+    await findAndClick("Run")
+    resolve()
+  }, 15000)
+ })
+ 
+
+
+}
 
 // (async () => {
-//   const handle = getWindowHandleByTitle(targetWindowTitle);
+//   // const handle = getWindowHandleByTitle(targetWindowTitle);
 //   // console.log(handle);
 
-//   if (handle !== 0) {
-//     const rect = getWindowRect(handle);
-//     console.log('Window Rect:', rect);
-//     const targetWord = 'File'; // Текст, который мы ищем
-
-//     const wordsWithCoordinates = await performOCRAndFindLines(rect);
-//     console.log('Слова с координатами:', wordsWithCoordinates);
-//     const foundObject = wordsWithCoordinates.find(
-//       obj => obj.text === targetWord
-//     );
-
-//     if (foundObject) {
-//       console.log('Найден объект:', foundObject);
-//       const { left, top } = foundObject.coordinates;
-//       robot.moveMouse(rect.left + left, rect.top + top);
-//       robot.mouseClick('left');
-//     } else {
-//       console.log('Объект с текстом', targetText, 'не найден.');
-//     }
+//   // if (handle !== 0) {
+//     // const rect = getWindowRect(handle);
+//     /
+   
 
 //     // if (textCoordinates) {
 //     //   console.log(
@@ -222,39 +318,43 @@ async function performOCRAndFindLines(rect) {
 
 //     // // Вызови функцию для клика по кнопке "File"
 //     // clickAt(fileButtonX, fileButtonY);
-//   } else {
-//     console.error('Window not found');
-//   }
+//   // } else {
+//   //   console.error('Window not found');
+//   // }
 // })();
 
-(async () => {
-  const handle = getWindowHandleByTitle(targetWindowTitle);
-  // console.log(handle);
+const intrId = setInterval(async ()=>{
+  const foundLines = await performOCRAndFindLines();
+  const targetLine = 'The project was successfully uploaded';
+  const targetLine2 = 'Failed to download the project';
+  const targetLine3 = 'There is no project here';
+  // 
+  // 
+  console.log('найденые строки:', foundLines);
 
-  if (handle !== 0) {
-    const rect = getWindowRect(handle);
-    console.log('Window Rect:', rect);
-    const targetWord = 'File'; // Текст, который мы ищем
+   if (foundLines.length <4) return
+   const noProject = false
 
-    const wordsWithCoordinates = await performOCRAndFindLines(rect);
-    console.log('', wordsWithCoordinates);
+  const successfull =     foundLines[foundLines.length-1].text.includes(targetLine) || 
+  foundLines[foundLines.length-2].text.includes(targetLine) || 
+  foundLines[foundLines.length-3].text.includes(targetLine) || 
+  foundLines[foundLines.length-4].text.includes(targetLine) 
+  const failedDownload =     foundLines[foundLines.length-1].text.includes(targetLine2) || 
+  foundLines[foundLines.length-2].text.includes(targetLine2) || 
+  foundLines[foundLines.length-3].text.includes(targetLine2) || 
+  foundLines[foundLines.length-4].text.includes(targetLine2) 
 
-    // if (textCoordinates) {
-    //   console.log(
-    //     `Найден текст "${targetText}" в координатах:`,
-    //     textCoordinates
-    //   );
-    //   // Твой код для дальнейших действий
-    // } else {
-    //   console.log(`Текст "${targetText}" не найден в заданной области.`);
-    // }
-    // Предположим, что координаты кнопки "File" внутри окна равны (x, y)
-    // const fileButtonX = rect.left + 50; // Замени на фактические значения
-    // const fileButtonY = rect.top + 50; // Замени на фактические значения
 
-    // // Вызови функцию для клика по кнопке "File"
-    // clickAt(fileButtonX, fileButtonY);
-  } else {
-    console.error('Window not found');
+
+
+  if (noProject) {
+    clearInterval(intrId)
+    return
   }
-})();
+  if (successfull || failedDownload) {
+    await runBot()
+   
+   
+    
+  }
+},60000)
---
+++ quant.js
@@ -0,0 +1,90 @@
+const { execSync } = require('child_process');
+
+function getWindowGeometryById(windowId) {
+  try {
+    const command = `xdotool getwindowgeometry ${windowId}`;
+    const result = execSync(command, { encoding: 'utf-8' });
+    const windowGeometry = result.trim();
+    return windowGeometry;
+  } catch (error) {
+    console.error(`Error while getting geometry for Window ID ${windowId}: ${error.message}`);
+    return null;
+  }
+}
+
+function getWindowIdByTitle(title) {
+  try {
+    const command = `xdotool search --name "${title}"`;
+    const result = execSync(command, { encoding: 'utf-8' });
+    const windowIds = result.trim().split('\n');
+    return windowIds;
+  } catch (error) {
+    console.error('Error:', error.message);
+    return [];
+  }
+}
+
+function findLargestWindow(title) {
+  const windowIds = getWindowIdByTitle(title);
+
+  let largestWindowId = null;
+  let largestWindowWidth = 0;
+  let largestWindowHeight = 0;
+
+  windowIds.forEach((windowId) => {
+    const windowGeometry = getWindowGeometryById(windowId);
+    console.log(`  Window Geometry: ${windowGeometry}`);
+
+    if (windowGeometry) {
+      // Разбираем строку с геометрией
+      const [, width, height] = windowGeometry.match(/Geometry:\s+(\d+)x(\d+)/) || [];
+      const currentWidth = parseInt(width, 10);
+      const currentHeight = parseInt(height, 10);
+
+      // Сравниваем с текущим наибольшим окном
+      if (currentWidth * currentHeight > largestWindowWidth * largestWindowHeight) {
+        largestWindowId = windowId;
+        largestWindowWidth = currentWidth;
+        largestWindowHeight = currentHeight;
+      }
+    }
+  });
+
+  return { id: largestWindowId, width: largestWindowWidth, height: largestWindowHeight };
+}
+
+function getWindowPositionById(windowId) {
+    const windowGeometry = getWindowGeometryById(windowId);
+  
+    if (windowGeometry) {
+      // Разбираем строку с геометрией и получаем позицию окна
+      const [, x, y] = windowGeometry.match(/Position:\s+(\d+),(\d+)/) || [];
+      
+      if (x !== undefined && y !== undefined) {
+        return { x: parseInt(x, 10), y: parseInt(y, 10) };
+      }
+    }
+  
+    return null;
+  }
+
+const title = 'Quant';
+
+const {id, width, height} = findLargestWindow(title);
+const windowPosition = getWindowPositionById(id)
+
+if (!id) {
+  console.log(`No windows found with title "${title}".`);
+  // console.log(qwe);
+} else {
+  
+}
+
+
+
+module.exports = {
+    windowPosition,
+    id,
+    width,
+    height
+  };
