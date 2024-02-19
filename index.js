75b7d972eb35c4e1adc823a61f9f9ce6c395f691
makfiz <99556605+makfiz@users.noreply.github.com>
2024-02-19 21:06:23 +0200

    dfd

--- index.js
+++ index.js
@@ -17,11 +17,23 @@ let botPosition;
 let downloadPosition;
 let runPosition;
 
+
+async function getWindowParam(title) {
+  try {
+    const { id, width, height } = findLargestWindow(title);
+    const windowPosition = getWindowPositionById(id);
+
+    return {windowPosition,width, height}
+  } catch (error) {
+    console.error('Произошла ошибка:', error);
+    throw error;
+  }
+}
+
 async function getQuantWindowParam() {
   const title = 'Quant';
   try {
-    const { id, width: newWidth, height: newHeight } = findLargestWindow(title);
-    const newPosition = getWindowPositionById(id);
+    const { windowPosition:newPosition, width: newWidth, height: newHeight } = await getWindowParam(title);
     windowPosition = newPosition;
     width = newWidth;
     height = newHeight;
@@ -31,15 +43,30 @@ async function getQuantWindowParam() {
   }
 }
 
+
+// async function getQuantWindowParam() {
+//   const title = 'Quant';
+//   try {
+//     const { id, width: newWidth, height: newHeight } = findLargestWindow(title);
+//     const newPosition = getWindowPositionById(id);
+//     windowPosition = newPosition;
+//     width = newWidth;
+//     height = newHeight;
+//   } catch (error) {
+//     console.error('Произошла ошибка:', error);
+//     throw error;
+//   }
+// }
+
 getQuantWindowParam();
 
-async function performOCRAndFindWords() {
+async function performOCRAndFindWords(p,w,h) {
   return new Promise((resolve, reject) => {
     const screen = robot.screen.capture(
-      windowPosition.x,
-      windowPosition.y - 50,
-      width,
-      height
+      p.x -10,
+      p.y -55 ,
+      w + 10,
+      h+30
     );
 
     new jimp(screen.width, screen.height, async function (err, img) {
@@ -85,18 +112,19 @@ async function performOCRAndFindWords() {
       // Удалите временный файл снимка
       fs.unlinkSync('screen.png');
       resolve(wordsWithCoordinates);
+     
       // return wordsWithCoordinates;
     });
   });
 }
 
-async function performOCRAndFindLines() {
+async function performOCRAndFindLines(p,w,h) {
   return new Promise((resolve, reject) => {
     const screen = robot.screen.capture(
-      windowPosition.x,
-      windowPosition.y - 50,
-      width,
-      height
+      p.x -10,
+      p.y -55 ,
+      w + 10,
+      h+30
     );
 
     new jimp(screen.width, screen.height, async function (err, img) {
@@ -164,7 +192,7 @@ async function findAndClick(name, position = undefined) {
       }, 1500);
     });
   } else {
-    const wordsWithCoordinates = await performOCRAndFindWords();
+    const wordsWithCoordinates = await performOCRAndFindWords(windowPosition,width,height);
     console.log('found strings splitet by word:', wordsWithCoordinates);
     const foundObject = wordsWithCoordinates.find(
       obj => obj.text === targetWord
@@ -219,72 +247,23 @@ function startObservation() {
   }, 60000);
 }
 
-// async function handleQuantStatus (id) {
-
-//   const lines = await performOCRAndFindLines();
-//   const foundLines = lines.filter(obj => obj.text.length >= 10);
-//   const targetLine = 'the project was successfully uploaded';
-//   const targetLine2 = 'failed to download the project';
-//   const targetLine3 = 'there is no project here';
-//   const targetLine4 = "there are no available projects"
-
-//   //
-//   //
-//   console.log('found strings:', foundLines);
-
-//    if (foundLines.length <4) return
-
-//   const successfull =     foundLines[foundLines.length-1].text.toLowerCase().includes(targetLine) ||
-//   foundLines[foundLines.length-2].text.toLowerCase().includes(targetLine) ||
-//   foundLines[foundLines.length-3].text.toLowerCase().includes(targetLine) ||
-//   foundLines[foundLines.length-4].text.toLowerCase().includes(targetLine)
-//   const failedDownload =     foundLines[foundLines.length-1].text.includes(targetLine2) ||
-//   foundLines[foundLines.length-2].text.toLowerCase().includes(targetLine2) ||
-//   foundLines[foundLines.length-3].text.toLowerCase().includes(targetLine2) ||
-//   foundLines[foundLines.length-4].text.toLowerCase().includes(targetLine2)
-
-//   const directoryEmpty =     foundLines[foundLines.length-1].text.includes(targetLine3) ||
-//   foundLines[foundLines.length-2].text.toLowerCase().includes(targetLine3) ||
-//   foundLines[foundLines.length-3].text.toLowerCase().includes(targetLine3) ||
-//   foundLines[foundLines.length-4].text.toLowerCase().includes(targetLine3)
-
-//   if (directoryEmpty) {
-//     console.log(`directoryEmpty ${directoryEmpty}`)
-//     const noProject = foundLines[foundLines.length-1].text.toLowerCase().includes(targetLine4) ||
-//     foundLines[foundLines.length-2].text.toLowerCase().includes(targetLine4) ||
-//     foundLines[foundLines.length-3].text.toLowerCase().includes(targetLine4) ||
-//     foundLines[foundLines.length-4].text.toLowerCase().includes(targetLine4)
-
-//     if (noProject) {
-//       console.log("no projects stop observation")
-//       clearInterval(id)
-//       watchingNow = false
-//       return
-//     } else {
-//       console.log("line 'There are no available projects' not found, run Bot")
-//       await runBot()
-//       return
-//     }
-
-//   }
-
-//   if (successfull || failedDownload ) {
-//     await runBot()
-//   }
-// }
 
 async function handleQuantStatus(id) {
-  const lines = await performOCRAndFindLines();
+  moveMouse(windowPosition.x, windowPosition.y);
+  mouseClick();
+  const lines = await performOCRAndFindLines(windowPosition,width,height);
   const foundLines = lines.filter(obj => obj.text.length >= 10);
   if (foundLines.length < 4) return;
-  const targetLines = foundLines.slice(-5);
+  const targetLines = foundLines.slice(-4);
 
   const targetLine = 'The project was successfully uploaded';
   const targetLine2 = 'Failed to download the project';
   const targetLine3 = 'There is no project here';
   const targetLine4 = 'There are no available projects';
   const targetLine5 = 'You need to re-enter the Quant';
-
+  const targetLine6 = "Lost internet connection"
+  const targetLine7 = "Unable to connect to platform"
+  // 
   //
   //
   console.log('found strings:', foundLines);
@@ -294,8 +273,17 @@ async function handleQuantStatus(id) {
       line.toLowerCase(),
       target.toLowerCase()
     );
-    return similarity >= 0.75; // Порог сходства 80%
+    return similarity >= 0.74; // Порог сходства 
   };
+
+
+  const internetConnection = foundLines.some(line =>
+    isSimilar(line.text, targetLine6)
+  );
+
+  const platformConnection = foundLines.some(line =>
+    isSimilar(line.text, targetLine7)
+  );
 
   const successfull = targetLines.some(line =>
     isSimilar(line.text, targetLine)
@@ -310,13 +298,41 @@ async function handleQuantStatus(id) {
     isSimilar(line.text, targetLine5)
   );
 
+  console.log('lostConnection', internetConnection || platformConnection);
   console.log('successfullyUploaded', successfull);
   console.log('failedDownload', failedDownload);
   console.log('directoryEmpty', directoryEmpty);
   console.log('needReOpenQuant', needReOpen);
 
+
+  if (internetConnection || platformConnection) {
+    const targetWord = 'Retry'
+    const { windowPosition, width, height} = await getWindowParam('Quant');
+    const wordsWithCoordinates = await performOCRAndFindWords(windowPosition,width,height);
+    console.log('wordsWithCoordinates:', wordsWithCoordinates);
+    
+    const foundObject = wordsWithCoordinates.find(obj =>
+      isSimilar(obj.text, targetWord))
+
+      console.log('foundObject:', foundObject);
+    if (foundObject) {
+     
+      const { left, top } = foundObject.coordinates;
+      await new Promise(resolve => {
+        moveMouse(windowPosition.x + left, windowPosition.y - 50 + top);
+        mouseClick();
+        setTimeout(() => {
+          resolve([windowPosition.x + left, windowPosition.y - 50 + top]);
+        }, 1500);
+      });
+    }
+    return
+  }
+
   if (needReOpen) {
+    clearInterval(id);
     reopenQuant()
+    return
   }
 
   if (directoryEmpty) {
@@ -342,7 +358,6 @@ async function handleQuantStatus(id) {
 }
 
 function reopenQuant() {
-  clearInterval(intrId);
   execSync(`killall Quant`);
   exec(`~/Quant/Quant`);
   setTimeout(async () => {
